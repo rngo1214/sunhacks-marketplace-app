55d6d40b5fa5163cbc9d2a671cbc8a5a4f482334
rngo1214 <rngo1214@gmail.com>
2018-11-10 09:19:54 -0700

    barebones login page

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+
+//Dimensions component gets the screen dimensions of the current device
+//Declared width and height variables to work with
+var width = Dimensions.get('window').width;
+var height = Dimensions.get('window').height;
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      field1: 'Insert Username', 
+      field2: 'Insert Password', 
+      text1: 'Insert Username', 
+      text2: 'Insert Password' };
+  }
+
   render() {
     return (
       <View style={styles.loginPage}>
-        <Text>Temporary App Name</Text>
-        <Text>Username</Text>
+        <Text style={styles.title}>Temporary App Name</Text>
+        <Text style={styles.whiteText}>Username</Text>
+        <TextInput style={styles.textField} onChangeText={(text1) => this.setState({text1})} placeholder={this.state.field1}></TextInput>
+        <Text style={styles.whiteText}>Password</Text>
+        <TextInput style={styles.textField} onChangeText={(text2) => this.setState({text2})} placeholder={this.state.field2}></TextInput>
+        <View style={{flex: 10}}></View>
       </View>
     );
   }
@@ -18,18 +36,36 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  }//,
-  // loginPage: {
-  //   flex: 3,
-  //   backgroundColor: '#f47a42',
-  //   alignItems: 'center',
-  // },
-  // whiteText: {
-  //   color: '#fff'
-  // },
-  // textField: {
-  //   color: '#fff',
-  //   borderColor: 'grey',
-  //   borderWidth: '1'
-  // }
+  },
+  loginPage: {
+    paddingTop: Expo.Constants.statusBarHeight,
+    flex: 1,
+    backgroundColor: '#f47a42',
+    alignItems: 'center'
+  },
+  title: {
+    flex: 1,
+    color: '#fff',
+    marginTop: 20,
+    marginBottom: 0,
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  whiteText: {
+    flex: 1,
+    color: '#fff',
+    marginTop: 20,
+    marginBottom: 0,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  textField: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'space-evenly',
+    textAlign: 'center',
+    width: width - (width / 10),
+    borderWidth: 1,
+    borderColor: 'grey'
+  }
 });
