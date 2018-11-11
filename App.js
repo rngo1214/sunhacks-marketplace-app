7a7a78d80de1d2e2a21bc5bfcf0263c120a72240
Atul Merchia <atulmerchia@gmail.com>
2018-11-11 05:32:54 -0700

    flatlist to db

--- App.js
+++ App.js
@@ -8,15 +8,45 @@ import Styles from './Styles'
 
 //To be moved to a separate .js file
 class LoginScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = { email: '', password: ''};
+
+    this.handlePress = this.handlePress.bind(this);
+  }
+
+  handlePress(){
+    var object = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+        email: this.state.email,
+        password: this.state.password
+      })
+    };
+
+    fetch('https://sunhacks-marketplace.herokuapp.com/login', object)
+    .then((response) => response.text())
+    .then((responseData) => {
+      // if(responseData != 'null')
+        this.props.navigation.navigate('Main');
+    })
+    .catch(function(err) {});
+
+  };
+
   render() {
     return (
       <View style={Styles.loginPage}>
         <Text style={Styles.title}>Temporary App Name</Text>
         <Text style={Styles.whiteText}>Email</Text>
-        <TextInput style={Styles.textField} placeholder={"Insert Email"}></TextInput>
+        <TextInput style={Styles.textField} placeholder={"Insert Email"} onChangeText={email => this.setState({email})}></TextInput>
         <Text style={Styles.whiteText}>Password</Text>
-        <TextInput style={Styles.textField} placeholder={"Insert Password"}></TextInput>
-        <TouchableHighlight style={Styles.loginButton} onPress={() => this.props.navigation.navigate('Main')} underlayColor={'grey'}>
+        <TextInput style={Styles.textField} placeholder={"Insert Password"} onChangeText={password => this.setState({password})}></TextInput>
+        <TouchableHighlight style={Styles.loginButton} onPress={this.handlePress} underlayColor={'grey'}>
           <Text>Login</Text>
         </TouchableHighlight>
         <View style={{flex: 5}}></View>
@@ -37,7 +67,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
-      
+
   render() {
     return (
       <RootStack />
--- Screens/BuyFeed.js
+++ Screens/BuyFeed.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList } from 'react-native';
+import Styles from '../Styles'
 
 export default class BuyFeed extends React.Component {
-    render() {
-        return (
-            <View>
-                <Text>Buy</Text>
-            </View>
-        )  
+  constructor(){
+    super();
+    this.state={
+      data: [],
     }
+  }
+
+  componentDidMount(){
+    var object = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+        col: 'buy-reqs',
+        search: {}
+      })
+    };
+
+    fetch('https://sunhacks-marketplace.herokuapp.com/getCards', object)
+    .then((response) => {
+      this.setState({
+        data: JSON.parse(response._bodyInit)
+      })
+      console.log(this.state.data);
+    })
+    .catch(function(err) {})
+  };
+
+  render() {
+    return (
+      <FlatList
+        keyExtractor={(item,index) => JSON.stringify(item._id)}
+        data={this.state.data}
+        renderItem={({item}) =>
+          <View style={{marginTop: 15, alignItems: 'center'}}>
+            <View style={Styles.card}>
+            <Text style={Styles.cardTitle}>{item.title}</Text>
+            <Text style={Styles.cardAuthor}>{item.author}</Text>
+            <Text style={Styles.cardDescription}>{item.description}</Text>
+            <Text style={Styles.cardBudget}>Budget: {item.budget}</Text>
+            </View>
+          </View>
+        }
+        style={{
+          backgroundColor: '#fff',
+        }}
+      />
+    )
+  }
 }
